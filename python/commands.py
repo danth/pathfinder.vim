1fe6e8e13f4a42cfbcbec0ebce67e18b85ad7491
Daniel Thwaites <danthwaites30@btinternet.com>
2020-05-29 10:08:54 +0100

    feat: explain what each motion does
    Show a description next to each step of the returned path, to help with
    learning new motions.

--- python/commands.py
+++ python/commands.py
@@ -17,11 +17,11 @@ def pathfinder_run():
     # Restore the cursor to where it was when :PathfinderBegin was ran
     winrestview(start)
 
-    output = ""
     for motion, group in itertools.groupby(motions):
         repetitions = len(list(group))
         # Add a count only if there is more than 1 repetition
-        output += str(repetitions) if repetitions > 1 else ""
-        output += motion.motion
+        motion_str = (str(repetitions) if repetitions > 1 else "") + motion.motion
 
-    print(output)
+        # Pad all motions to 5 characters wide so the descriptions are aligned
+        padding = " " * (5 - len(motion_str))
+        print(motion_str, motion.description(repetitions), sep=padding)
--- python/pathfinder.py
+++ python/pathfinder.py
@@ -9,6 +9,10 @@ class Motion:
     def __init__(self, dict):
         self.motion = dict["motion"].decode()
         self.weight = dict["weight"]
+        self.description_template = dict["description"].decode()
+
+    def description(self, count):
+        return self.description_template.format(count=count)
 
 
 def update_preexisting_node(node, preexisting_node, open_nodes):
