89d9b1fbecdd2f8c57dae0bfeecf90bba99f3901
Daniel Thwaites <danthwaites30@btinternet.com>
2020-05-30 13:09:00 +0100

    refactor: move display_results outside of pathfinder_run

--- python/commands.py
+++ python/commands.py
@@ -14,6 +14,23 @@ def pathfinder_begin():
     print("Move to target location and then :PathfinderRun")
 
 
+def display_results(motions):
+    output = ""
+    for motion, group in itertools.groupby(motions):
+        repetitions = len(list(group))
+        # Add a count only if there is more than 1 repetition
+        # Builds a string like "2k", "15w"
+        motion_str = (str(repetitions) if repetitions > 1 else "") + motion.motion
+
+        # Pad all motions to 5 characters wide so the descriptions are aligned
+        padding = " " * (5 - len(motion_str))
+        output += motion_str + padding + motion.description(repetitions) + "\n"
+
+    # Printing multiple lines doesn't make a hit-enter prompt appear so we
+    # pass the string to the echo command instead
+    # replace(', \') escapes ' characters
+    vim.command("echo \"" + output.replace("'", "\\'") + '"')
+
 def pathfinder_run():
     if start is None:
         return print("Please run :PathfinderBegin to set a start position first")
@@ -21,21 +38,5 @@ def pathfinder_run():
     target = winsaveview()
     if cursor_in_same_position(start, target):
         return print("No motions required")
-
-    def display_results(motions):
-        output = ""
-        for motion, group in itertools.groupby(motions):
-            repetitions = len(list(group))
-            # Add a count only if there is more than 1 repetition
-            motion_str = (str(repetitions) if repetitions > 1 else "") + motion.motion
-            # Pad all motions to 5 characters wide so the descriptions are aligned
-            padding = " " * (5 - len(motion_str))
-
-            output += motion_str + padding + motion.description(repetitions) + "\n"
-
-        # Printing multiple lines doesn't make a hit-enter prompt appear so we
-        # pass the string to the echo command instead
-        # replace(', \') escapes ' characters
-        vim.command("echo \"" + output.replace("'", "\\'") + '"')
 
     client.pathfind(start, target, display_results)
