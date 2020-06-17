5e2e49f8553d730ad68b760546a38197d5e2e9c3
Daniel Thwaites <danthwaites30@btinternet.com>
2020-06-17 23:45:46 +0100

    build: fix files missing from release archive

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+__version__ = "1.7.0"
+
+
+setup(
+    name="pathfinder.vim",
+    version=__version__,
+    author="Daniel Thwaites",
+    author_email="danthwaites30@btinternet.com",
+    url="https://github.com/AlphaMycelium/pathfinder.vim",
+)
