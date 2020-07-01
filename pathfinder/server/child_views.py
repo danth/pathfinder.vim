ed5d50269dcaea2076f86a03d26b2c396b68219f
Daniel Thwaites <danthwaites30@btinternet.com>
2020-07-01 13:40:34 +0100

    fix: prevent exception when searching T motions

--- pathfinder/server/child_views.py
+++ pathfinder/server/child_views.py
@@ -17,18 +17,18 @@ def _ftFT(view, motion):
 
     # characters = string of characters which may be accessible using this motion
     # column = lambda function which converts index in `characters` to a column number
-    if motion.motion == "f" and view.col <= len(line_text):
+    if motion.motion == "f" and view.col < len(line_text):
         column = lambda i: view.col + i + 1
         characters = line_text[view.col + 1 :]
     elif motion.motion == "t" and view.col < len(line_text) - 1:
         column = lambda i: view.col + i + 1
         characters = line_text[view.col + 2 :]
         seen_characters.add(line_text[view.col + 1])
-    elif motion.motion == "F" and view.col > 0:
+    elif motion.motion == "F" and view.col > 0 and len(line_text) > view.col:
         column = lambda i: view.col - i - 1
         # Characters are reversed because we are looking backwards
         characters = line_text[: view.col][::-1]
-    elif motion.motion == "T" and view.col > 1:
+    elif motion.motion == "T" and view.col > 1 and len(line_text) > view.col:
         column = lambda i: view.col - i - 1
         characters = line_text[: view.col - 1][::-1]
         seen_characters.add(line_text[view.col - 1])
