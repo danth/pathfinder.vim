09e106a5dd8d72aa7e6b292157795d9c4ad4878f
Daniel Thwaites <danthwaites30@btinternet.com>
2020-08-11 23:33:17 +0100

    test: update tests for 41343f0f32915a71dc0b8d085d4da360b9e1330a

--- tests/test_output.py
+++ tests/test_output.py
@@ -1,7 +1,8 @@
 import pytest
+from unittest import mock
 
 from pathfinder.client.output import compact_motions, explained_motions, get_count
-from pathfinder.motion import Motion
+from pathfinder.server.motions import Motion
 
 COUNTS = [
     ("j", 1, "j"),
@@ -14,20 +15,27 @@ COUNTS = [
 
 @pytest.mark.parametrize("motion,count,expected", COUNTS, ids=[x[2] for x in COUNTS])
 def test_get_count(motion, count, expected):
-    motion = Motion(motion, motion, 1, "")
+    motion = Motion(motion, None)
     assert get_count(motion, count) == expected
 
 
 def test_compact_motions():
-    motion1 = Motion("j", "j", 1, "")
-    motion2 = Motion("k", "k", 1, "")
+    motion1 = Motion("j", None)
+    motion2 = Motion("k", None)
     assert compact_motions([motion1, motion2, motion2, motion2]) == "j 3k"
 
 
 def test_explained_motions():
-    motion1 = Motion("j", "j", 1, "Down {count} lines")
-    motion2 = Motion("k", "k", 1, "Up {count} lines")
-    assert list(explained_motions([motion1, motion2, motion2])) == [
-        " j  Down 1 lines",
-        "kk  Up 2 lines",
-    ]
+    motion1 = Motion("j", None)
+    motion2 = Motion("f", "g")
+    with mock.patch(
+        "pathfinder.client.output.vim.vars",
+        {"pf_descriptions": {
+            "j": "Down {count} lines",
+            "f": "To occurence {count} of {argument}"
+        }}
+    ):
+        assert list(explained_motions([motion1, motion2, motion2])) == [
+            "j  Down 1 lines",
+            "2fg  To occurence 2 of g",
+        ]
